f118955388dbe8da8f6de9e65900d4b0818d0659
Alan Douglas A de Souza <alan.douglas.ads@gmail.com>
2022-03-31 19:49:14 -0300

    Refact: refact searcScript code

--- _Scripts/searchScript.js
+++ _Scripts/searchScript.js
@@ -3,10 +3,11 @@ window.onload = () => {
     // Pegando o parâmetro de cnpj da url
     const urlParams = new URLSearchParams(window.location.search);
     const getCnpj = urlParams.get('cnpj');
+    const cnpjOk = getCnpj.replace('/', '*');
 
     // Consultando a API com o parametro cnpj para retornar a pesquisa       
     try {
-        oneCompany(getCnpj);
+        oneCompany(cnpjOk);
     } catch (err) {
         console.error(`Erro ao procurar cadastro: ${err}`);
     }        
@@ -16,24 +17,24 @@ window.onload = () => {
 const oneCompany = async (cnpj) => {
     const title = document.getElementById('iu');
     try {            
-        const result = await fetch(`http://localhost:8080/findOneCompany/${cnpj}`);
-        const data = await result.json();                       
+        const result = await fetch(`http://localhost:8080/findCompanyByCnpj/${cnpj}`);
+        const tupla = await result.json();        
             creates({
-                name: data.name,
-                cnpj: '00000000/00',
-                phone: '0444444444',
-                insta: 'insta alguma coisa',
-                email: 'um.mail.com',
-                adress: 'algum lugar',
-                complement: 'alguma coisa',
-                causes: 'food',
-                director: 'Alguem',
-                abstract: 'ajdhnasdsanbkask',
-                inscEs: '415644163464',
-                inscMu: '326323625232',
-                face: 'facekjksn',
-                site: 'www.lololo.com',
-                photo: data.photoCompany
+                name: tupla.name,
+                cnpj: tupla.companies_information.cnpj,
+                phone: tupla.companies_information.phone,
+                insta: tupla.companies_information.instagram,
+                email: tupla.companies_information.email,
+                adress: tupla.companies_information.adress,
+                complement: tupla.companies_information.complement,
+                causes: tupla.companies_information.causes,
+                director: tupla.companies_information.director,
+                abstract: tupla.abstract,
+                inscEs: tupla.companies_information.insc_est,
+                inscMu: tupla.companies_information.insc_mun,
+                face: tupla.companies_information.facebook,
+                site: tupla.companies_information.website,
+                photo: tupla.photoCompany
             })            
     } catch (err) {
         console.error(`Erro ao consultar API: ${err}`);
